407df61346acbab96a8f518f9ce6a4f9a9f58105
Spyman <wtispy@gmail.com>
2014-09-14 22:01:52 +0400

    Добавлен контроллер для страницы обработчика, доработки интерфейса, теперь страница обработчик наконец понимает пришедший в нее запрос, запрос должен быть вид страница/переменна/ т.е. статический что не вяжется с <form> решения пока нет.

--- src/main/java/com/simbircite/demo/controller/IndexController.java
+++ src/main/java/com/simbircite/demo/controller/IndexController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class IndexController {
 
     @RequestMapping(value = "", method = RequestMethod.GET)
-    public String index() {
-        return "redirect:/users";
+    public String actions() {
+        return "reports";
     }
 }
